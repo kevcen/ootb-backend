01f019840cd6ee4dba6d25899baca1a3e549880d
Jack Q <jljqiuc@gmail.com>
2021-06-03 14:40:20 +0100

    Fix test.ts to include done keyword

--- test/test.ts
+++ test/test.ts
@@ -2,9 +2,9 @@ import request from 'supertest';
 import app from '../app';
 
 describe('App', function() {
-  it('has the default page', function() {
+  it('has the default page', function(done) {
     request(app)
       .get('/')
-      .expect("This is the home endpoint");
+      .expect("This is the home endpoint", done);
   });
 }); 

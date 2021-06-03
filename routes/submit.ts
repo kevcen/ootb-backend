fdcc081c83126708c9fd87fb051205733cbafcea
Jack Q <jljqiuc@gmail.com>
2021-06-03 14:40:48 +0100

    Add submit route with test

--- routes/submit.ts
+++ routes/submit.ts
@@ -8,7 +8,7 @@ enum Gender {
 }
 
 /* Submit a profile of giftee so that gift recommendations can be generated and sent */
-router.post('/submit', function(req, res, next) {
+router.post('/', function(req, res) {
   var interest : string[] = req.body.interest;
   var gender : Gender = req.body.gender;
   var minPrice : Number = req.body.minPrice;
@@ -19,7 +19,7 @@ router.post('/submit', function(req, res, next) {
   //var itemList = getGifts(interest, gender, minPrice, maxPrice)
   
   var itemList : [string[], string[]] = [[], []]; // [list of image links, list of product page links]
-  res.end(JSON.stringify(itemList));
+  res.send(JSON.stringify(itemList));
 });
 
 export default router
---
+++ test/submit.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('Submit', function() {
+  it('has the default page', function(done) {
+    request(app)
+      .post('/submit')
+      .expect("[[],[]]", done);
+  });
+}); 

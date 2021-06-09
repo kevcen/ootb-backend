8853dcca5f7d164f9ca96c0c78fe3020f3369666
kev <kev9821@hotmail.co.uk>
2021-06-09 21:44:43 +0100

    filtered gender, relationship and cost for products

--- routes/products.ts
+++ routes/products.ts
@@ -2,19 +2,48 @@ import express from "express";
 import Category from "../db_models/Category";
 import Item from "../db_models/Item";
 import Product from "../db_models/Product";
+import Sequelize from "sequelize";
 var router = express.Router();
+const Op = Sequelize.Op;
 
 /* POST simple get products by categories */
 router.post("/", async (req, res, next) => {
   try {
     var categories: string[] = req.body.categories || [];
+    var price: string[] = req.body.price || [];
+    var relationship: string = req.body.relationship || "any";
+    var gender: string = req.body.gender || "any"; 
+    gender = ["Prefer not to say", "Other"].includes(gender) ? "any" : gender; 
+    var occasion: string = req.body.occasion || "any";
+
     var products: Product[] = await Product.findAll({
       include: [
         {
           model: Category,
-          where: { name: categories },
+          where: { 
+            name: categories,
+            gender: {
+              [Op.or]: {
+                [Op.eq]: gender,
+                [Op.eq]: "any"
+              }
+            },
+            relationship: {
+              [Op.or]: {
+                [Op.eq]: relationship,
+                [Op.eq]: "any"
+              }
+            }
+          },
+          // TODO: use, relationship, occasion
         },{
-          model : Item
+          model : Item,
+          where: {
+            cost: {
+              [Op.lte]: price[1],
+              [Op.gte]: price[0]
+            }
+          }
         }
       ],
     });

717838239526a31a1fd75934f0c1644af18d40e3
Jack Q <jljqiuc@gmail.com>
2021-06-14 21:13:55 +0100

    Add POST request for submitting search queries

--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,39 @@
 import express from "express";
+import User from "../db_models/User";
+const { Op } = require("sequelize");
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.send("This is the user endpoint");
 });
 
-export default router
+router.post("/search", async (req, res, next) => {
+  var searchValue = req.body.searchValue;
+
+  var users: User[] = await User.findAll({
+    where: {
+      [Op.or]: [
+        {
+          firstname: {
+            [Op.like]: "%" + searchValue + "%",
+          },
+        },
+        {
+          lastname: {
+            [Op.like]: "%" + searchValue + "%",
+          },
+        },
+        {
+          username: {
+            [Op.like]: "%" + searchValue + "%",
+          },
+        },
+      ],
+    },
+  });
+  res.send(users);
+});
+
+export default router;
